92873c9323904791ecad456367c4e6bc66c372a8
Dusch4593 <brandondusch@gmail.com>
2020-08-25 17:25:59 -0400

    added more pages to go with new menuLinks in gatsby-config

---
+++ src/pages/blog.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+
+
+class Blog extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Blog" />
+      </Layout>
+    )
+  }
+}
+
+export default Blog
---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+
+
+class Contact extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Contact" />
+      </Layout>
+    )
+  }
+}
+
+export default Contact
