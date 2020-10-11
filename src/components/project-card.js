b40e453bd00e4a34104a8b49c58aa57fb6d1aad6
Dusch4593 <brandondusch@gmail.com>
2020-10-11 09:20:27 -0400

    created project card component and populated relevant data in data/projects.json

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Image from 'gatsby-image'
 
 
-const ProjectCard = ({ slug }) => {
+const ProjectCard = ({ demoURL, githubURL, imageData, title, description }) => {
   return(
     <div className="project-card">
-      <Link to={`/${slug}`}></Link>
+      <Image className="project-image" fluid={ imageData } alt={ title } />
+      <div className="project-info">
+        <h2>{ title }</h2>
+        <p>{ description }</p>
+        <a href={demoURL} rel="noopener noreferrer" target="_blank">View the demo</a> &nbsp;
+        <a href={githubURL} rel="noopener noreferrer" target="_blank">View the source</a>
+        <br/><br/><br/>
+      </div>
     </div>
   )
 }
