7a019695860e8311bdd2c9c4dfe7e3b53672aaa1
Dusch4593 <brandondusch@gmail.com>
2021-03-17 09:56:29 -0400

    working on color contract between background and text

--- src/components/blogPost.tsx
+++ src/components/blogPost.tsx
@@ -15,7 +15,7 @@ const BlogPost = ({ title, date, link, thumbnail }: BlogPostProps) => {
   return(
     < React.Fragment>
       <div className={blogPostStyles.blogPostCard}>
-        <a href={link}><img className={blogPostStyles.blogPostImage} src={ thumbnail } alt={ title } /></a>
+        <img className={blogPostStyles.blogPostImage} src={ thumbnail } alt={ title } />
         <div className={blogPostStyles.blogPostInfo}>
             <a href={link}><h4>{ title }</h4></a>
             <p>Published on: <strong>{date}</strong></p>
