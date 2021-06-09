c1ccfd2d67684ce522fd3950068c59ae574efb06
Dusch4593 <brandondusch@gmail.com>
2021-06-09 09:25:08 -0400

    fix: revise relative path passed to src in Static Image

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -1,31 +1,37 @@
 import * as React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // @ts-ignore
 import * as aboutStyles from '../styles/about.module.css'
 
 const About = () => {
-    const query = useStaticQuery(graphql`query MyQuery {
-  file(relativePath: {eq: "images/portfolio-site-image.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(
-        width: 375
-        height: 500
-        placeholder: BLURRED
-        layout: CONSTRAINED
-      )
-    }
-  }
-}
-`)
+  //   const query = useStaticQuery(graphql`query MyQuery {
+  //   file(relativePath: {eq: }) {
+  //     childImageSharp {
+  //       gatsbyImageData(
+  //         width: 375
+  //         height: 500
+  //         placeholder: BLURRED
+  //         layout: CONSTRAINED
+  //       )
+  //     }
+  //   }
+  // }
+  // `)
   
-    const imageData = query.file.childImageSharp.gatsbyImageData.src
+    // const imageData = query.file.childImageSharp.gatsbyImageData.src
 
     return (
         <React.Fragment>
             <div className={aboutStyles.aboutWrapper}>
                 <div className={aboutStyles.aboutInfo }>
-                    <img className={aboutStyles.aboutImage} srcSet={imageData} alt='Brandon Dusch'/>
+                    {/* <img className={aboutStyles.aboutImage} srcSet={imageData} alt='Brandon Dusch'/> */}
+                    <StaticImage 
+                      src="../../data/images/portfolio-site-image.jpg"
+                      alt="Brandon Dusch"
+                      className={aboutStyles.aboutImage}
+                    />
                     <div className={aboutStyles.aboutText}>
                     <h2>Hi! I'm Brandon </h2>
                     <p>Creative full stack software engineer with a passion for learning and problem-solving. My attraction to tech stems from my prior background in music and public libraries, where I learned to work with patterns and build on top of them. I want to continue to grow and improve my new craft, and change the world for the better while doing it.</p>
